38e7408526a697cb5bc5c66059413d31fbeee257
Garry-82 <168720206+Garry-82@users.noreply.github.com>
2024-05-14 15:35:18 +0300

    Add files via upload

--- HomeWork2.py
+++ HomeWork2.py
@@ -1,14 +1,3 @@
-# list_=["one", "two", "three", "four"]
-# print(list_)
-# for i in range(len(list_)):
-#     list_[i] = ":)"
-# print(list_)
-#
-# for i in range(1,11):
-#     for j in range (1,11):
-#         print(f"{i} * {j} = {i*j}")
-#     print('----------')
-
 cars_list=['BMW','MB','LADA','KIA','HONDA']
 for i in cars_list:
     print("я езжу на автомобиле марки ", i)
